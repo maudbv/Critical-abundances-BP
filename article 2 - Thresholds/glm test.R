0dd53f2f898cfcf0651aed0c72aab0d5f6785d67
unknown <bernarm2@MB9176.lincoln.ac.nz>
2015-02-17 15:48:07 +1300

    Revert "README.txt"
    This reverts commit f9831b0280eae09b6cd49d90ae0f7ae4c0e0d83c.

--- article 2 - Thresholds/glm test.R
+++ article 2 - Thresholds/glm test.R
@@ -206,7 +206,7 @@ if (!bootstrap) return(list(glms=glms, thresh = thresh,  spearman=spear, n.obs =
 
 
 # summarizing results on thresholds per group of species
-summary.glmtest <- function(M = glmSR.grass,data=species, group="ALIEN", type =c("glm", "boot")) {
+summary.glmtest = function(M = glmSR.grass,data=species, group="ALIEN", type =c("glm", "boot")) {
   
   ### select onlys species in the model
   data=data[rownames(M$dif),]
@@ -215,78 +215,70 @@ summary.glmtest <- function(M = glmSR.grass,data=species, group="ALIEN", type =c
   
   ## divide species according to grouping factor (ie Alien vs Native)
   if (is.null(group)) {
-    sub<-M
-    G<-1
+    sub=M
+    G=1
   }
   
   if (!is.null(group)) { 
-    sub<-list()
-    i<-1
-    G<-length(unique(data[,group]))
+    sub=list()
+    i=1
+    G=length(unique(data[,group]))
     for (g in sort(unique(data[,group]))) {
-      sub[[i]]<-lapply(M, FUN= function(x) x[rownames(x) %in% rownames(data)[data[,group]==g],] )
-      i<-i+1
+      sub[[i]]=lapply(M, FUN= function(x) x[rownames(x) %in% rownames(data)[data[,group]==g],] )
+      i=i+1
     }
-    names<-paste(group, ":",sort(unique(data[,group])), sep="")
+    names=paste(group, ":",sort(unique(data[,group])), sep="")
   }                                 
   
-  out<-NULL   
-  sum.table <- NULL 
+  out=NULL   
   
   for (j in 1:G) {
-    S<-sub[[j]]
+    S=sub[[j]]
     
     # number of potentieally signif species occurrence per class
-    n.sp<-sapply(names(S$dif), FUN= function(x) {
+    n.sp=sapply(names(S$dif), FUN= function(x) {
       length(which(!is.na(S$dif[,x])))
     })
     
     # number of species which have a threshold per class
-    n.target<-sapply(names(S$dif), FUN= function(x) {
+    n.target=sapply(names(S$dif), FUN= function(x) {
       length(which(!is.na(S$dif[,x]) & !is.na(S$thresh$th)) )
     })
     
     
     # number of significant negative effects per class
-    n.negative<-sapply(names(S$dif), FUN= function(x) {
+    n.negative=sapply(names(S$dif), FUN= function(x) {
       length(which(S$P[,x]<=0.05 & S$z[,x]<0))
     })
     
     
     # number of significant positive effects per class
-    n.positive<-sapply(names(S$dif), FUN= function(x) {
+    n.positive=sapply(names(S$dif), FUN= function(x) {
       length(which(S$P[,x]<0.05 & S$z[,x]>=0))
     })
     
     # number of significant negative impavts per class for species with a threshld
-    n.impact<-sapply(names(S$dif), FUN= function(x) {
+    n.impact=sapply(names(S$dif), FUN= function(x) {
       length(which(S$P[,x]<=0.05 & S$z[,x]<0 & !is.na(S$thresh$th)))
     })
     
     # proportion of signifi negative effects per class
-    p.impact<-n.impact/ n.sp
+    p.impact=n.impact/ n.sp
     
     # proportion of signifi negative effects per class
-    p.negative<-n.negative/ n.sp
+    p.negative=n.negative/ n.sp
     
     # number of times each class is the threshold
-    freq.thr <-table(as.factor(S$thresh$th))[match(2:6,names(table(as.factor(S$thresh$th))))]
-    names(freq.thr)<-names(n.sp)
-    freq.thr[is.na(freq.thr)]<-0
-    prop.thr <- freq.thr / n.sp
-    if(sum(freq.thr, na.rm=T)!=0) perc.thr <- freq.thr / sum(freq.thr, na.rm=T)
-    if(sum(freq.thr, na.rm=T)==0) perc.thr <- freq.thr 
+    freq.thr =table(as.factor(S$thresh$th))[match(2:6,names(table(as.factor(S$thresh$th))))]
+    names(freq.thr)=names(n.sp)
+    freq.thr[is.na(freq.thr)]=0
+    prop.thr = freq.thr / n.sp
+    if(sum(freq.thr, na.rm=T)!=0) perc.thr = freq.thr / sum(freq.thr, na.rm=T)
+    if(sum(freq.thr, na.rm=T)==0) perc.thr = freq.thr 
     
-    out<-rbind(out, data.frame(group=names[j], nb.sp=n.sp, n.target = n.target, freq.impact=n.impact, freq.negative =n.negative,freq.positive =n.positive,
+    out=rbind(out, data.frame(group=names[j], nb.sp=n.sp, n.target = n.target, freq.impact=n.impact, freq.negative =n.negative,freq.positive =n.positive,
                               prop.impact=as.array(p.impact),freq.thr=freq.thr, prop.thr=prop.thr, perc.thr=perc.thr))
-    
-    #summary table
-    n.sp <- length(rownames(S$thresh))
-    freq.thr <- length(which(!is.na(S$thresh$th)))
-       
-    sum.table <-rbind(sum.table,  data.frame(group=names[j], nb.sp=n.sp,  freq.thr=freq.thr))
-
   }
-  return(list(class.summary =out, overall.summary = sum.table))
+  return(out)
 }
 
