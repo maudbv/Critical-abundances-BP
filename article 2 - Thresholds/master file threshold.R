897d2948e56e71f0b79bc4f73d40c6d9cd5aae53
unknown <bernarm2@MB9176.lincoln.ac.nz>
2015-02-17 13:40:29 +1300

    added summary table of total number of thresholds per focal group of species

--- article 2 - Thresholds/master file threshold.R
+++ article 2 - Thresholds/master file threshold.R
@@ -29,16 +29,28 @@ R=999
 db=databp[databp$PlotName %in% realgrasslands,]
 
 # perform analyses
-# system.time(glmSR.grass <- glm.test(db = db,bootstrap = T, R=R))
-# glmSRnat.grass <- glm.test(db = db,var="SRnat",bootstrap = T, R=R)
-# glmSRali.grass <- glm.test(db = db,var="SRali",bootstrap = T, R=R)
+#  system.time(glmSR.grass <- glm.test(db = db,bootstrap = T, R=R))
+#  glmSRnat.grass <- glm.test(db = db,var="SRnat",bootstrap = T, R=R)
+#  glmSRali.grass <- glm.test(db = db,var="SRali",bootstrap = T, R=R)
 # save(glmSR.grass,glmSRnat.grass,glmSRali.grass, file = "saved Rdata/article 2 - threshold/booststrapped.glms.Rdata")
-load(file = "saved Rdata/article 2 - threshold/booststrapped.glms.Rdata")
+# 
+ load(file = "saved Rdata/article 2 - threshold/booststrapped.glms.Rdata")
+# 
+# system.time(glmSR.grass <- glm.test(db = db,bootstrap = F, R=R))
+# glmSRnat.grass <- glm.test(db = db,var="SRnat",bootstrap = F, R=R)
+# glmSRali.grass <- glm.test(db = db,var="SRali",bootstrap = F, R=R)
+# save(glmSR.grass,glmSRnat.grass,glmSRali.grass, file = "saved Rdata/article 2 - threshold/non-booststrapped.glms.Rdata")
+
+system.time(glmSR.grass <- glm.test(db = db,bootstrap = T, R=R, covar ="dominance"))
+glmSRnat.grass <- glm.test(db = db,var="SRnat",bootstrap = T, R=R, covar ="dominance")
+glmSRali.grass <- glm.test(db = db,var="SRali",bootstrap = T, R=R, covar ="dominance")
+save(glmSR.grass,glmSRnat.grass,glmSRali.grass, file = "saved Rdata/article 2 - threshold/booststrapped+dominance.glms.Rdata")
 
 ### frequencies of significantly negative effects and thresholds
-effects.glmSR.grass <-  summary.glmtest(M=glmSR.grass, group="ALIEN")
-effects.glmSRnat.grass <- summary.glmtest(M=glmSRnat.grass, group="ALIEN") 
-effects.glmSRali.grass <- summary.glmtest(M=glmSRali.grass, group="ALIEN") 
+effects.glmSR.grass <-  summary.glmtest(M=glmSR.grass, group="ALIEN", type="boot")
+effects.glmSRnat.grass <- summary.glmtest(M=glmSRnat.grass, group="ALIEN", type="boot") 
+effects.glmSRali.grass <- summary.glmtest(M=glmSRali.grass, group="ALIEN", type="boot") 
 
 # save results
 save.image("saved Rdata/article 2 - threshold/article threshold 1.0.Rdata")
+load("saved Rdata/article 2 - threshold/article threshold 1.0.Rdata")
