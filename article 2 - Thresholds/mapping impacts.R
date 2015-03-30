41eda32e06b920f8122661bcbba9f355ef05a1d3
unknown <bernarm2@MB9176.lincoln.ac.nz>
2015-03-30 19:21:59 +1300

    recuperation file problematiques

--- article 2 - Thresholds/mapping impacts.R
+++ article 2 - Thresholds/mapping impacts.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ### Mapping impacts
 
 
@@ -34,3 +35,41 @@ for( i in targets) {
   points(envplot[impact, c("POINTX", "POINTY")],pch=22,  col="firebrick", bg="firebrick")
 }
 
+=======
+### Mapping impacts
+
+
+### mapping landcover
+landcover.col=c("grey","grey",
+                "grey","grey",
+                "red","grey",
+                "goldenrod","grey",
+                "forestgreen","grey",
+                "grey","grey",
+                "grey","grey",
+                "grey","grey",
+                "grey", "grey")
+par(mar=c(1,1,1,1))
+plot(envplot$POINTX,envplot$POINTY,pch=22,xlim=c(min(envplot$POINTX), 2550000),
+     col=landcover.col[as.numeric(as.factor(envplot$landcover))], bg=landcover.col[as.numeric(as.factor(envplot$landcover))])
+legend('topright',legend= c("High productivity grassland", 
+                            "Low productivity grassland", "Gorse and Broom","other"),
+       fill=c("goldenrod","forestgreen","red","grey"), cex=0.6)
+
+
+### plot species i am looking for
+db=databp[databp$PlotName %in% realgrasslands,]
+targets <- rownames(glmSRnat.grass$boot.thresh)[which(!is.na(glmSRnat.grass$boot.thresh$th))]
+
+par(mfrow=c(3,5), mar=c(1,2,3,1))                         
+for( i in targets) {
+   plot.alien=as.character(db[which(db$abun%in%c(1,2,3,4,5,6) & db$vegtype=="G" & db$SpeciesCode==i),"PlotName" ])  
+  th= glmSRnat.grass$boot.thresh[i, "th"]
+  impact=as.character(db[which(db$abun>= th & db$vegtype=="G" & db$SpeciesCode==i),"PlotName" ])
+  
+  plot(envplot$POINTX,envplot$POINTY,pch=22,main=i,col='lightgrey', bg="lightgrey",  axes=F)
+  points(envplot[plot.alien, c("POINTX", "POINTY")],pch=22, col="olivedrab",bg="olivedrab")
+  points(envplot[impact, c("POINTX", "POINTY")],pch=22,  col="firebrick", bg="firebrick")
+}
+
+>>>>>>> Post-doc-lincoln/master
